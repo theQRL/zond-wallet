3b1f74f40d9c1661c4656e26203244846d5473b7
Shaheer Shukur <shaheershukur@gmail.com>
2024-06-07 17:05:43 +0400

    feat: adds get all accounts

--- functions/getAllAccounts.js
+++ functions/getAllAccounts.js
@@ -1,11 +1,12 @@
 /**
  *
  * @param {import("@theqrl/web3").Web3ZondInterface} zondInstance
- * @returns {void}
+ * @returns {string[]}
  */
 const getAllAccounts = async (zondInstance) => {
   const allAccounts = await zondInstance.getAccounts();
   console.log(">>> getAllAccounts:\n", allAccounts);
+  return allAccounts;
 };
 
 module.exports = { getAllAccounts };
