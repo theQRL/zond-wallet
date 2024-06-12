e2dcdf6bf75fc79809c07c5dcf2bb5d5b783fe5a
Shaheer Shukur <shaheershukur@gmail.com>
2024-06-13 01:13:44 +0400

    feat: adds create new account

--- src/functions/getAllAccountBalances.ts
+++ src/functions/getAllAccountBalances.ts
@@ -2,14 +2,14 @@ import { Web3ZondInterface } from "@theqrl/web3";
 import { getAllAccounts } from "./getAllAccounts";
 
 export const getAllAccountBalances = async (
-  zondInstance: Web3ZondInterface
+  zondInstance: Web3ZondInterface,
 ) => {
   const allAccounts = await getAllAccounts(zondInstance);
   const allAccountBalances = await Promise.all(
     allAccounts.map(async (account) => {
       const balance = await zondInstance.getBalance(account);
       return `${account}: ${balance}`;
-    })
+    }),
   );
   console.log(">>> getAllAccountBalances:\n", allAccountBalances);
   return allAccountBalances;
