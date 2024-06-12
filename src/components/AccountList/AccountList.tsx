a163e977bbaddb8a1dfa7e3ce0b5f100af4ce449
Shaheer Shukur <shaheershukur@gmail.com>
2024-06-13 00:56:39 +0400

    feat: adds new account

--- src/components/AccountList/AccountList.tsx
+++ src/components/AccountList/AccountList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchAccounts } from "../../functions/zond";
 import { Account } from "./Account";
+import { NewAccount } from "./NewAccount";
 
 export const AccountList = () => {
   const [accountsList, setAccoutsList] = useState<string[]>();
@@ -18,6 +19,7 @@ export const AccountList = () => {
 
   return (
     <div className="flex flex-col gap-8 p-8">
+      <NewAccount />
       {accountsList?.map((account) => (
         <Account key={account} account={account} />
       ))}
