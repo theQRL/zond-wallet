017c89b04886b3741abd72b8ee8f8fa8008e62f6
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-01 13:02:56 +0400

    feat: adds zond fetchnetwork

--- src/functions/zond.ts
+++ src/functions/zond.ts
@@ -2,10 +2,14 @@ import { Web3 } from "@theqrl/web3";
 import { zondConfig } from "../configuration/zondConfig";
 import { getAllAccounts } from "./getAllAccounts";
 const zondHttpProvider = new Web3.providers.HttpProvider(
-  zondConfig.zondHttpProvider
+  zondConfig.zondHttpProvider,
 );
 const web3 = new Web3(zondHttpProvider);
 const zondInstance = web3.zond;
+
+export const fetchNetwork = async () => {
+  return await zondInstance.net.isListening();
+};
 
 export const fetchAccounts = async () => {
   return await getAllAccounts(zondInstance);
