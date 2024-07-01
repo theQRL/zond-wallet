7b8f4af5de0ddce96403acc3a0f34cf7f64d811c
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-01 12:20:20 +0400

    feat: adds useNetwork hook

---
+++ src/hooks/useNetwork.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { fetchNetwork } from "../functions/zond";
+
+export const useNetwork = () => {
+  const [hasNetworkConnection, setHasNetworkConnection] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const zondNetworkStatus = fetchNetwork();
+        setHasNetworkConnection(zondNetworkStatus);
+      } catch (err) {
+        console.log("Error occured when fetching network status");
+      }
+    })();
+  }, []);
+
+  return { hasNetworkConnection };
+};
