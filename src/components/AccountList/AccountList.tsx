cec81415367b7bc801061c28d5ba9f234b79b58d
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-01 16:43:04 +0400

    refactor: updates account list

--- src/components/AccountList/AccountList.tsx
+++ src/components/AccountList/AccountList.tsx
@@ -9,6 +9,9 @@ export const AccountList = () => {
     <div className="flex flex-col gap-8 p-8">
       <NewAccount />
       <div className="flex flex-col gap-4">
+        <span>Current account</span>
+        <Account key={234} account={"0xTestAccount"} />
+        <span>Other available accounts</span>
         {accountsList?.map((account) => (
           <Account key={account} account={account} />
         ))}
--- src/components/AccountList/NewAccount.tsx
+++ src/components/AccountList/NewAccount.tsx
@@ -1,7 +1,7 @@
 export const NewAccount = () => {
   return (
     <div className="cursor-pointer rounded-xl border-2 border-secondary bg-primary p-2 font-bold text-secondary transition-colors delay-75 hover:bg-secondary hover:text-primary">
-      New Account
+      Add new account
     </div>
   );
 };
