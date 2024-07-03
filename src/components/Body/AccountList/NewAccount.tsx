9a412020422913be42404b5a9c0b84a4d8ba1beb
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-03 21:10:04 +0400

    feat: updates add new account button

--- src/components/Body/AccountList/NewAccount.tsx
+++ src/components/Body/AccountList/NewAccount.tsx
@@ -1,7 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export const NewAccount = () => {
-  return (
-    <div className="cursor-pointer rounded-xl border-2 border-secondary p-2 font-bold transition-colors delay-75">
-      Add new account
-    </div>
-  );
+  return <Button>Add new account</Button>;
 };
