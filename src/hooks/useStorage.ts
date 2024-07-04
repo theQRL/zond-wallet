f1a2cf631df9bb69ae001ad18128a7dcb8e919da
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-04 16:56:47 +0400

    feat: adds storage hook

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
-export const useStorage = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
+const ACTIVE_ACCOUNT_IDENTIFIER = "ACTIVE_ACCOUNT";
 
-  return { currentAccount, setCurrentAccount };
+export const useStorage = () => {
+  const [currentAccount, setCurrentAccount] = useState(
+    localStorage.getItem(ACTIVE_ACCOUNT_IDENTIFIER) ?? "",
+  );
+
+  const setActiveAccount = (account: string) => {
+    localStorage.setItem(ACTIVE_ACCOUNT_IDENTIFIER, account);
+    setCurrentAccount(account);
+  };
+
+  return { activeAccount: currentAccount, setActiveAccount };
 };
