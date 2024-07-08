ebd7cbe31401ac7621a2110c86aceab39de37d1e
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-08 16:36:02 +0400

    fix: fixes port number

--- src/configuration/zondConfig.ts
+++ src/configuration/zondConfig.ts
@@ -1,6 +1,6 @@
 const ZOND_PROVIDER = {
   DEV: { url: "http://localhost:8545", name: "Zond Local Node" },
-  TEST_NET: { url: "http://209.250.255.226:3500", name: "Zond Testnet" },
+  TEST_NET: { url: "http://209.250.255.226:8545", name: "Zond Testnet" },
   MAIN_NET: { url: "https://mainnet.zond.com", name: "Zond Mainnet" },
 };
 
