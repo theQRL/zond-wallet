099f6a540ca3fb4f8fff2dbd52656fb37e82d925
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-09 13:58:28 +0400

    feat: updates zond store

--- src/stores/zondStore.ts
+++ src/stores/zondStore.ts
@@ -1,11 +1,45 @@
-import { makeAutoObservable, observable } from "mobx";
+import { zondConfig } from "@/configuration/zondConfig";
+import Web3 from "@theqrl/web3";
+import { action, makeAutoObservable, observable, runInAction } from "mobx";
+
+const { name, url } = zondConfig.zondHttpProvider;
+const zondHttpProvider = new Web3.providers.HttpProvider(url);
+const web3 = new Web3(zondHttpProvider);
 
 class ZondStore {
-  isDarkMode;
+  zondNetworkName = name;
+  zondInstance = web3.zond;
+  zondConnection = { isConnected: false, isLoading: false };
 
   constructor() {
-    makeAutoObservable(this, { isDarkMode: observable });
-    this.isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    makeAutoObservable(this, {
+      zondNetworkName: observable,
+      zondInstance: observable.struct,
+      zondConnection: observable.struct,
+      fetchZondConnection: action.bound,
+    });
+    this.fetchZondConnection();
+  }
+
+  async fetchZondConnection() {
+    this.zondConnection = { ...this.zondConnection, isLoading: true };
+    try {
+      const isListening = await this.zondInstance.net.isListening();
+      runInAction(() => {
+        this.zondConnection = {
+          ...this.zondConnection,
+          isConnected: isListening,
+        };
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.zondConnection = { ...this.zondConnection, isConnected: false };
+      });
+    } finally {
+      runInAction(() => {
+        this.zondConnection = { ...this.zondConnection, isLoading: false };
+      });
+    }
   }
 }
 
