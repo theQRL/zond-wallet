276b910382e435baf3b851402f35e8e4deb26c09
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-09 15:26:35 +0400

    fix: updates other accounts

--- src/components/Body/AccountList/OtherAccounts/OtherAccounts.tsx
+++ src/components/Body/AccountList/OtherAccounts/OtherAccounts.tsx
@@ -1,17 +1,17 @@
 import { Account } from "@/components/Body/AccountList/Account/Account";
 import { Label } from "@/components/ui/label";
-import { useAcccountBalances } from "@/hooks/useAccountBalances";
 import { useStore } from "@/stores/store";
 import { observer } from "mobx-react-lite";
 
 export const OtherAccounts = observer(() => {
-  const { accountBalances } = useAcccountBalances();
-  const { accountStore } = useStore();
+  const { accountStore, zondStore } = useStore();
   const { activeAccount, setActiveAccount } = accountStore;
   const { accountAddress } = activeAccount;
+  const { zondAccounts } = zondStore;
+  const { accounts } = zondAccounts;
 
   const otherAccountsLabel = `${accountAddress ? "Other accounts" : "Accounts"} in the wallet`;
-  const otherAccounts = Object.keys(accountBalances).filter(
+  const otherAccounts = Object.keys(accounts).filter(
     (account) => account !== accountAddress,
   );
 
