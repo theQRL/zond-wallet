825f424ef1961e166d5be7c8d19a3774b7788939
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-09 15:27:01 +0400

    feat: adds fetchAccounts in zond store

--- src/stores/zondStore.ts
+++ src/stores/zondStore.ts
@@ -1,4 +1,5 @@
 import { zondConfig } from "@/configuration/zondConfig";
+import { getAllAccounts } from "@/functions/getAllAccounts";
 import Web3 from "@theqrl/web3";
 import { action, makeAutoObservable, observable, runInAction } from "mobx";
 
@@ -6,16 +7,23 @@ const { name, url } = zondConfig.zondHttpProvider;
 const zondHttpProvider = new Web3.providers.HttpProvider(url);
 const web3 = new Web3(zondHttpProvider);
 
+type ZondAccountType = {
+  accounts: string[];
+  isLoading: boolean;
+};
+
 class ZondStore {
   zondNetworkName = name;
   zondInstance = web3.zond;
   zondConnection = { isConnected: false, isLoading: false };
+  zondAccounts: ZondAccountType = { accounts: [], isLoading: false };
 
   constructor() {
     makeAutoObservable(this, {
       zondNetworkName: observable,
       zondInstance: observable.struct,
       zondConnection: observable.struct,
+      zondAccounts: observable.struct,
       fetchZondConnection: action.bound,
     });
     this.fetchZondConnection();
@@ -40,6 +48,28 @@ class ZondStore {
         this.zondConnection = { ...this.zondConnection, isLoading: false };
       });
     }
+  }
+
+  async fetchAccounts() {
+    this.zondAccounts = { ...this.zondAccounts, isLoading: true };
+    try {
+      const accounts = await this.zondInstance.getAccounts();
+      runInAction(() => {
+        this.zondAccounts = {
+          ...this.zondAccounts,
+          accounts: accounts,
+        };
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.zondAccounts = { ...this.zondAccounts };
+      });
+    } finally {
+      runInAction(() => {
+        this.zondAccounts = { ...this.zondAccounts, isLoading: false };
+      });
+    }
+    return await getAllAccounts(this.zondInstance);
   }
 }
 
