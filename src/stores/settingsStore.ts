f3310877e4598845da0b167d4dc379980376f56d
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-11 15:55:02 +0400

    feat: updates settings store for theme

--- src/stores/settingsStore.ts
+++ src/stores/settingsStore.ts
@@ -1,19 +1,19 @@
 import { makeAutoObservable, observable } from "mobx";
 
-const THEME = {
+const THEME = Object.freeze({
   DARK: "dark",
   LIGHT: "light",
-};
+});
 
 class SettingsStore {
   isDarkMode;
+  theme;
 
   constructor() {
-    makeAutoObservable(this, { isDarkMode: observable });
+    makeAutoObservable(this, { isDarkMode: observable, theme: observable });
     this.isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    document?.documentElement?.classList?.add(
-      this.isDarkMode ? THEME.DARK : THEME.LIGHT,
-    );
+    this.theme = this.isDarkMode ? THEME.DARK : THEME.LIGHT;
+    document?.documentElement?.classList?.add(this.theme);
   }
 }
 
