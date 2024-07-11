ab618a6d102aa975773954e4d93c3dc6ab32c67e
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-11 14:03:15 +0400

    feat: adds theme in settings store

--- src/stores/settingsStore.ts
+++ src/stores/settingsStore.ts
@@ -1,11 +1,19 @@
 import { makeAutoObservable, observable } from "mobx";
 
+const THEME = {
+  DARK: "dark",
+  LIGHT: "light",
+};
+
 class SettingsStore {
   isDarkMode;
 
   constructor() {
     makeAutoObservable(this, { isDarkMode: observable });
     this.isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    document?.documentElement?.classList?.add(
+      this.isDarkMode ? THEME.DARK : THEME.LIGHT,
+    );
   }
 }
 
