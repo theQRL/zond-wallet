fa045d32ba9531c3aeccac9c86fc1a17f1f7c9dd
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-11 19:44:08 +0400

    fix: sets spacing for home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -13,7 +13,7 @@ export const Home = observer(() => {
   return (
     <div className="relative text-foreground">
       <BackgroundVideo />
-      <div className="absolute top-0 flex w-full flex-col items-center gap-4 pt-36">
+      <div className="absolute top-0 flex w-full flex-col items-center gap-4 pt-28">
         <img className="h-16 w-16" src="icon.png" />
         {isLoading ? (
           <Loader className="animate-spin text-foreground" size="32" />
