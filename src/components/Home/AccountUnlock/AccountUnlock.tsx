8b18f86bb094af92da654fe9cffb7fe124576a52
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-12 15:19:58 +0400

    feat: updates account unlock component

--- src/components/Home/AccountUnlock/AccountUnlock.tsx
+++ src/components/Home/AccountUnlock/AccountUnlock.tsx
@@ -7,34 +7,105 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/UI/Card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/UI/Form";
 import { Input } from "@/components/UI/Input";
 import { useStore } from "@/stores/store";
-import { LockKeyholeOpen } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader, LockKeyholeOpen } from "lucide-react";
 import { observer } from "mobx-react-lite";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  password: z.string().min(1),
+});
 
 export const AccountUnlock = observer(() => {
   const { accountStore } = useStore();
   const { activeAccount } = accountStore;
   const { accountAddress } = activeAccount;
 
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    reValidateMode: "onSubmit",
+    defaultValues: {
+      password: "",
+    },
+  });
+  const {
+    handleSubmit,
+    control,
+    formState: { isSubmitting, isValid },
+  } = form;
+
+  async function onSubmit(formData: z.infer<typeof FormSchema>) {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log(JSON.stringify(formData, null, 2));
+
+    const unlocked = false;
+    if (unlocked) {
+    } else {
+      control.setError("password", {
+        message: "The entered password is incorrect",
+      });
+    }
+  }
+
   return (
-    <Card className="w-80">
-      <CardHeader>
-        <CardTitle>
-          Unlock account {accountAddress.substring(accountAddress.length - 5)}
-        </CardTitle>
-        <CardDescription className="break-words">
-          {accountAddress}
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <Input type="password" />
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full" type="submit">
-          <LockKeyholeOpen className="mr-2 h-4 w-4" /> Unlock
-        </Button>
-      </CardFooter>
-    </Card>
+    <Form {...form}>
+      <form className="w-80" onSubmit={handleSubmit(onSubmit)}>
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              Unlock 0x..{accountAddress.substring(accountAddress.length - 5)}
+            </CardTitle>
+            <CardDescription className="break-words">
+              {accountAddress}
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="Password"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>Enter the account password</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button
+              disabled={isSubmitting || !isValid}
+              className="w-full"
+              type="submit"
+            >
+              {isSubmitting ? (
+                <Loader className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                <LockKeyholeOpen className="mr-2 h-4 w-4" />
+              )}
+              Unlock{isSubmitting ? "ing" : ""}
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
   );
 });
