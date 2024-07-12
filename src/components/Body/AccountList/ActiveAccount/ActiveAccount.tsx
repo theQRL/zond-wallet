30494c21160da8801eae20b92fa395389d419855
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-12 19:34:29 +0400

    refactor: refactors accounts card items

--- src/components/Body/AccountList/ActiveAccount/ActiveAccount.tsx
+++ src/components/Body/AccountList/ActiveAccount/ActiveAccount.tsx
@@ -1,30 +1,67 @@
+import { Button } from "@/components/UI/Button";
+import { Card } from "@/components/UI/Card";
 import { Label } from "@/components/UI/Label";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/UI/Tooltip";
 import { useStore } from "@/stores/store";
+import { Copy } from "lucide-react";
 import { observer } from "mobx-react-lite";
-import { AccountCard } from "../AccountCard/AccountCard";
 
 export const ActiveAccount = observer(() => {
   const { accountStore } = useStore();
   const { activeAccount } = accountStore;
   const { accountAddress } = activeAccount;
 
+  const splitLength = 5;
+  const prefix = accountAddress.substring(0, 2);
+  const idSplit = [];
+  for (let i = 2; i < accountAddress.length; i += splitLength) {
+    idSplit.push(accountAddress.substring(i, i + splitLength));
+  }
+
   const activeAccountLabel = `${accountAddress ? "Active account" : ""}`;
 
-  const copyAccount = (account: string) => {
-    navigator.clipboard.writeText(account);
+  const copyAccount = () => {
+    navigator.clipboard.writeText(accountAddress);
   };
 
   return (
     !!accountAddress && (
       <>
         <Label className="text-secondary">{activeAccountLabel}</Label>
-        <AccountCard
-          onClickHandler={() => copyAccount(accountAddress)}
-          buttonVariant="ghost"
-          key={accountAddress}
-          account={accountAddress}
-          accountStatus="active"
-        />
+        <Card className="flex h-min gap-2 p-4 font-bold text-foreground hover:bg-accent">
+          <div className="flex gap-2">
+            <div>{prefix}</div>
+            <div className="flex flex-wrap gap-1 font-bold">
+              {idSplit.map((part) => (
+                <div key={part}>{part}</div>
+              ))}
+            </div>
+          </div>
+          <span>
+            <TooltipProvider>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger asChild>
+                  <Button
+                    className="hover:text-secondary"
+                    variant="outline"
+                    size="icon"
+                    onClick={() => copyAccount()}
+                  >
+                    <Copy size="18" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent side="bottom">
+                  <Label>Copy Address</Label>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </span>
+        </Card>
       </>
     )
   );
--- src/components/Body/AccountList/OtherAccounts/OtherAccounts.tsx
+++ src/components/Body/AccountList/OtherAccounts/OtherAccounts.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/UI/Button";
+import { Card } from "@/components/UI/Card";
 import { Label } from "@/components/UI/Label";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/UI/Tooltip";
 import { useStore } from "@/stores/store";
+import { ArrowRight } from "lucide-react";
 import { observer } from "mobx-react-lite";
-import { AccountCard } from "../AccountCard/AccountCard";
 
 export const OtherAccounts = observer(() => {
   const { accountStore, zondStore } = useStore();
@@ -9,6 +17,13 @@ export const OtherAccounts = observer(() => {
   const { accountAddress: activeAccountAddress } = activeAccount;
   const { zondAccounts } = zondStore;
   const { accounts } = zondAccounts;
+
+  const splitLength = 5;
+  const prefix = activeAccountAddress.substring(0, 2);
+  const idSplit: string[] = [];
+  for (let i = 2; i < activeAccountAddress.length; i += splitLength) {
+    idSplit.push(activeAccountAddress.substring(i, i + splitLength));
+  }
 
   const otherAccountsLabel = `${activeAccountAddress ? "Other accounts" : "Accounts"} in the wallet`;
   const otherAccounts = accounts.filter(
@@ -20,13 +35,35 @@ export const OtherAccounts = observer(() => {
       <>
         <Label className="text-secondary">{otherAccountsLabel}</Label>
         {otherAccounts.map(({ accountAddress }) => (
-          <AccountCard
-            onClickHandler={() => setActiveAccount(accountAddress)}
-            buttonVariant="outline"
-            key={accountAddress}
-            account={accountAddress}
-            accountStatus="other"
-          />
+          <Card className="flex h-min gap-2 p-4 font-bold text-foreground hover:bg-accent">
+            <div className="flex gap-2">
+              <div>{prefix}</div>
+              <div className="flex flex-wrap gap-1">
+                {idSplit.map((part) => (
+                  <div key={part}>{part}</div>
+                ))}
+              </div>
+            </div>
+            <span>
+              <TooltipProvider>
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <Button
+                      className="hover:text-secondary"
+                      variant="outline"
+                      size="icon"
+                      onClick={() => setActiveAccount(accountAddress)}
+                    >
+                      <ArrowRight size="18" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent side="bottom">
+                    <Label>Switch to this account</Label>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </span>
+          </Card>
         ))}
       </>
     )
