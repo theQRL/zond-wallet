c87f08130d4c0fa75e8825812d020941be98f9e0
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-15 23:21:24 +0400

    feat: updates connection badge

--- src/components/Home/ConnectionBadge/ConnectionBadge.tsx
+++ src/components/Home/ConnectionBadge/ConnectionBadge.tsx
@@ -1,6 +1,19 @@
+import { Button } from "@/components/UI/Button";
 import { Card } from "@/components/UI/Card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/UI/DropdownMenu";
+import { ZOND_PROVIDER } from "@/configuration/zondConfig";
 import { useStore } from "@/stores/store";
 import { cva } from "class-variance-authority";
+import { Check, ChevronDown, HardDrive, Network, Workflow } from "lucide-react";
 import { observer } from "mobx-react-lite";
 
 const networkStatusClasses = cva("h-2 w-2 rounded-full", {
@@ -15,19 +28,87 @@ const networkStatusClasses = cva("h-2 w-2 rounded-full", {
   },
 });
 
+const blockchainSelectionClasses = cva("cursor-pointer", {
+  variants: {
+    isSelected: {
+      true: ["text-constructive"],
+    },
+  },
+  defaultVariants: {
+    isSelected: false,
+  },
+});
+
 export const ConnectionBadge = observer(() => {
   const { zondStore } = useStore();
-  const { zondConnection, zondNetworkName } = zondStore;
-  const { isConnected } = zondConnection;
+  const { zondConnection } = zondStore;
+  const { isConnected, zondNetworkName } = zondConnection;
+
+  const { DEV, TEST_NET, MAIN_NET } = ZOND_PROVIDER;
+  const [isDevNetwork, isTestNetwork, isMainNetwork] = [
+    DEV.name === zondNetworkName,
+    TEST_NET.name === zondNetworkName,
+    MAIN_NET.name === zondNetworkName,
+  ];
 
   return (
-    <Card className="flex items-center gap-2 rounded-full px-4 py-2">
-      <Card
-        className={networkStatusClasses({
-          networkStatus: isConnected,
-        })}
-      />
-      {zondNetworkName}
-    </Card>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-2 rounded-full">
+          <Card
+            className={networkStatusClasses({
+              networkStatus: isConnected,
+            })}
+          />
+          {zondNetworkName}
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Blockchain network</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem
+            className={blockchainSelectionClasses({
+              isSelected: isDevNetwork,
+            })}
+          >
+            <HardDrive className="mr-2 h-4 w-4" />
+            <span>{DEV.name}</span>
+            {isDevNetwork && (
+              <DropdownMenuShortcut>
+                <Check className="h-4 w-4" />
+              </DropdownMenuShortcut>
+            )}
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className={blockchainSelectionClasses({
+              isSelected: isTestNetwork,
+            })}
+          >
+            <Workflow className="mr-2 h-4 w-4" />
+            <span>{TEST_NET.name}</span>
+            {isTestNetwork && (
+              <DropdownMenuShortcut>
+                <Check className="h-4 w-4" />
+              </DropdownMenuShortcut>
+            )}
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className={blockchainSelectionClasses({
+              isSelected: isMainNetwork,
+            })}
+          >
+            <Network className="mr-2 h-4 w-4" />
+            <span>{MAIN_NET.name}</span>
+            {isMainNetwork && (
+              <DropdownMenuShortcut>
+                <Check className="h-4 w-4" />
+              </DropdownMenuShortcut>
+            )}
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 });
