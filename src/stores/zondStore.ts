7e78dc7f449a8623e3de739ab6ea825e3fd10392
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-17 19:22:52 +0400

    feat: updates zond store

--- src/stores/zondStore.ts
+++ src/stores/zondStore.ts
@@ -4,8 +4,13 @@ import { action, makeAutoObservable, observable, runInAction } from "mobx";
 
 const BLOCKCHAIN_SELECTION_IDENTIFIER = "BLOCKCHAIN_SELECTION";
 const DEFAULT_BLOCKCHAIN = ZOND_PROVIDER.TEST_NET.id;
+const ACTIVE_ACCOUNT_IDENTIFIER = "ACTIVE_ACCOUNT";
 
 type BlockchainType = keyof typeof ZOND_PROVIDER;
+
+type ActiveAccountType = {
+  accountAddress: string;
+};
 
 type ZondAccountType = {
   accountAddress: string;
@@ -23,14 +28,17 @@ class ZondStore {
     isConnected: false,
     isLoading: false,
     zondNetworkName: "",
+    zondNetworkId: "",
   };
   zondAccounts: ZondAccountsType = { accounts: [], isLoading: false };
+  activeAccount: ActiveAccountType = { accountAddress: "" };
 
   constructor() {
     makeAutoObservable(this, {
       zondInstance: observable.struct,
       zondConnection: observable.struct,
       zondAccounts: observable.struct,
+      activeAccount: observable.struct,
       selectBlockchain: action.bound,
       fetchZondConnection: action.bound,
       fetchAccounts: action.bound,
@@ -45,10 +53,23 @@ class ZondStore {
     ) ?? DEFAULT_BLOCKCHAIN) as BlockchainType;
 
     const { name, url } = ZOND_PROVIDER[selectedBlockChain];
-    this.zondConnection = { ...this.zondConnection, zondNetworkName: name };
+    this.zondConnection = {
+      ...this.zondConnection,
+      zondNetworkName: name,
+      zondNetworkId: selectedBlockChain,
+    };
     const zondHttpProvider = new Web3.providers.HttpProvider(url);
     const { zond } = new Web3(zondHttpProvider);
     this.zondInstance = zond;
+
+    const storedActiveAccount =
+      localStorage.getItem(
+        `${this.zondConnection.zondNetworkId}${ACTIVE_ACCOUNT_IDENTIFIER}`,
+      ) ?? "";
+    this.activeAccount = {
+      ...this.activeAccount,
+      accountAddress: storedActiveAccount,
+    };
 
     this.fetchZondConnection();
     this.fetchAccounts();
@@ -57,6 +78,13 @@ class ZondStore {
   selectBlockchain(selectedBlockchain: string) {
     localStorage.setItem(BLOCKCHAIN_SELECTION_IDENTIFIER, selectedBlockchain);
     this.initializeBlockchain();
+  }
+
+  setActiveAccount(activeAccount?: string) {
+    localStorage.setItem(
+      `${this.zondConnection.zondNetworkId}${ACTIVE_ACCOUNT_IDENTIFIER}`,
+      activeAccount ?? "",
+    );
   }
 
   async fetchZondConnection() {
