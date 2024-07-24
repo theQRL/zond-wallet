e020ae3c34abb68bb9365b411444d0db89a0bd40
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-24 11:54:37 +0400

    feat: adds copy mnemonic option

--- src/components/ZondWallet/Body/Home/CreateAccount/MnemonicDisplay/MnemonicDisplay.tsx
+++ src/components/ZondWallet/Body/Home/CreateAccount/MnemonicDisplay/MnemonicDisplay.tsx
@@ -8,6 +8,8 @@ import {
   CardTitle,
 } from "@/components/UI/Card";
 import { getMnemonicFromHexSeed } from "@/functions/getMnemonicFromHexSeed";
+import { ArrowRight, Copy } from "lucide-react";
+import { useEffect, useState } from "react";
 import { MnemonicWordListing } from "./MnemonicWordListing/MnemonicWordListing";
 
 export const MnemonicDisplay = () => {
@@ -15,6 +17,26 @@ export const MnemonicDisplay = () => {
   const mnemonic = getMnemonicFromHexSeed(
     "0x3500827651de73ea580916b19b042d0656a5ed33cdd09a4c481b000c7606d7aa3f6d7f9cff94fea393230b31a8087bbf",
   );
+
+  const [hasJustCopied, setHasJustCopied] = useState(false);
+  const [timer, setTimer] = useState<NodeJS.Timeout>();
+
+  useEffect(() => {
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [timer]);
+
+  const onCopy = () => {
+    setHasJustCopied(true);
+    navigator.clipboard.writeText(mnemonic);
+    const newTimer = setTimeout(() => {
+      setHasJustCopied(false);
+    }, 1000);
+    setTimer(newTimer);
+  };
 
   const cardDescription = `Don't lose this. You may need this someday to import or recover your account ${account.substring(0, 5)}...${account.substring(account.length - 5)}`;
 
@@ -27,8 +49,13 @@ export const MnemonicDisplay = () => {
       <CardContent>
         <MnemonicWordListing mnemonic={mnemonic} />
       </CardContent>
-      <CardFooter>
+      <CardFooter className="gap-4">
+        <Button className="w-full" type="button" onClick={onCopy}>
+          <Copy className="mr-2 h-4 w-4" />
+          {hasJustCopied ? "Copied" : "Copy"}
+        </Button>
         <Button className="w-full" type="submit">
+          <ArrowRight className="mr-2 h-4 w-4" />
           Continue
         </Button>
       </CardFooter>
