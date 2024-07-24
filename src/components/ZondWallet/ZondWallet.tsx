f869de44bc68ba08e743f47dcfdc81f294173556
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-24 10:17:32 +0400

    feat: adds scroll to top on route change

--- src/components/ZondWallet/ZondWallet.tsx
+++ src/components/ZondWallet/ZondWallet.tsx
@@ -1,10 +1,12 @@
 import { Body } from "@/components/ZondWallet/Body/Body";
 import { Header } from "@/components/ZondWallet/Header/Header";
+import ScrollToTop from "@/components/ZondWallet/ScrollToTop/ScrollToTop";
 import { observer } from "mobx-react-lite";
 
 export const ZondWallet = observer(() => {
   return (
     <div className="flex min-h-[48rem] w-96 flex-col overflow-x-hidden bg-background text-foreground">
+      <ScrollToTop />
       <Header />
       <Body />
     </div>
