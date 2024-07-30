6c221f89e2c6f0fc09f6df8623ddcf8af8e9500b
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-30 11:00:59 +0400

    build: rebuilds source

--- Extension/assets/Label-EVIep7GS.js
+++
@@ -1,6 +0,0 @@
-import{c,d,a as n}from"./createLucideIcon-BaD5mpES.js";import{r as o,j as s}from"./index-DdhxihET.js";import{P as f}from"./index-Da7hFKZi.js";/**
- * @license lucide-react v0.400.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const w=c("Copy",[["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2",key:"17jyea"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2",key:"zix9uf"}]]);var m="Label",i=o.forwardRef((a,t)=>s.jsx(f.label,{...a,ref:t,onMouseDown:e=>{var r;e.target.closest("button, input, select, textarea")||((r=a.onMouseDown)==null||r.call(a,e),!e.defaultPrevented&&e.detail>1&&e.preventDefault())}}));i.displayName=m;var l=i;const u=n("text-sm text-foreground font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),x=o.forwardRef(({className:a,...t},e)=>s.jsx(l,{ref:e,className:d(u(),a),...t}));x.displayName=l.displayName;export{w as C,x as L};
