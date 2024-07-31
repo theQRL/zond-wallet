f67662703b631555c8cea3600b9091eedda49e45
Shaheer Shukur <shaheershukur@gmail.com>
2024-07-31 14:55:51 +0400

    chore: removes unused function

--- src/functions/getAllAccounts.ts
+++
@@ -1,7 +0,0 @@
-import { Web3ZondInterface } from "@theqrl/web3";
-
-export const getAllAccounts = async (zondInstance: Web3ZondInterface) => {
-  const allAccounts = await zondInstance.getAccounts();
-  console.log(">>> getAllAccounts:\n", allAccounts);
-  return allAccounts;
-};
