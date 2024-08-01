4fed112b88fbc288c929de634c734d4272646c57
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-01 14:06:28 +0400

    fix: updates create button icon

--- src/components/ZondWallet/Body/AccountList/NewAccount/NewAccount.tsx
+++ src/components/ZondWallet/Body/AccountList/NewAccount/NewAccount.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/components/UI/Button";
 import { ROUTES } from "@/router/router";
-import { Wallet } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export const NewAccount = () => {
   return (
     <Link to={ROUTES.HOME}>
       <Button className="flex w-full gap-2">
-        <Wallet size="18" /> Create or import an account
+        <Plus size="18" /> Create or import an account
       </Button>
     </Link>
   );
