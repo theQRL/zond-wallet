91702d5fd8963a5e00ae3c792a24317a115a11f8
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-05 14:50:07 +0400

    feat: adds blockchain selection to storage util

--- src/utilities/storageUtil.ts
+++ src/utilities/storageUtil.ts
@@ -1,9 +1,28 @@
+import { ZOND_PROVIDER } from "@/configuration/zondConfig";
+
 const ACTIVE_PAGE_IDENTIFIER = "ACTIVE_PAGE";
+const BLOCKCHAIN_SELECTION_IDENTIFIER = "BLOCKCHAIN_SELECTION";
+
+type BlockchainType = keyof typeof ZOND_PROVIDER;
 
 /**
  * A utility for storing and retrieving states of different components to and from the browser storage.
  */
 class StorageUtil {
+  /**
+   * A function for storing the blockchain selection.
+   * Call the getBlockChain function to retrieve the stored value.
+   */
+  static setBlockChain(selectedBlockchain: string) {
+    localStorage.setItem(BLOCKCHAIN_SELECTION_IDENTIFIER, selectedBlockchain);
+  }
+
+  static getBlockChain() {
+    const DEFAULT_BLOCKCHAIN = ZOND_PROVIDER.TEST_NET.id;
+    return (localStorage.getItem(BLOCKCHAIN_SELECTION_IDENTIFIER) ??
+      DEFAULT_BLOCKCHAIN) as BlockchainType;
+  }
+
   /**
    * A function for storing the route to be displayed on opening the extension.
    * Call the getActivePage function to retrieve the stored value, and clearActivePage for clearing the stored value.
