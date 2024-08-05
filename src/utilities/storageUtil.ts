1a995e9968f015d83c3db05efb91835bc9c2ad84
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-05 14:11:26 +0400

    docs: adds doc for active page setting

--- src/utilities/storageUtil.ts
+++ src/utilities/storageUtil.ts
@@ -1,6 +1,13 @@
 const ACTIVE_PAGE_IDENTIFIER = "ACTIVE_PAGE";
 
+/**
+ * A utility for storing and retrieving states of different components to and from the browser storage.
+ */
 class StorageUtil {
+  /**
+   * A function for storing the route to be displayed on opening the extension.
+   * Call the getActivePage function to retrieve the stored value, and clearActivePage for clearing the stored value.
+   */
   static setActivePage(activePage?: string) {
     if (activePage) {
       localStorage.setItem(ACTIVE_PAGE_IDENTIFIER, activePage);
