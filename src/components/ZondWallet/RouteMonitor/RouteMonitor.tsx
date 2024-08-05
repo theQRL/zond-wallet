61274adc1f7f35a1873c2b4f9453c29610ca05fd
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-05 12:08:20 +0400

    refactor: renames route monitor

--- src/components/ZondWallet/RouteMonitor/RouteMonitor.tsx
+++ src/components/ZondWallet/RouteMonitor/RouteMonitor.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-export default function ScrollToTop() {
+export default function RouteMonitor() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
