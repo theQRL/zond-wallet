fd9c94bf9173e9a65f61b6bd22ae1f191677f549
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-05 13:39:34 +0400

    docs: adds doc for RouteMonitor

--- src/components/ZondWallet/RouteMonitor/RouteMonitor.tsx
+++ src/components/ZondWallet/RouteMonitor/RouteMonitor.tsx
@@ -2,6 +2,10 @@ import StorageUtil from "@/utilities/storageUtil";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
+/**
+ * An empty component that stays inside the ZondWallet component to watch for changing routes.
+ * This component takes care of scrolling the screen to top on route change and ensures the user is redirected to the same page on opening the extension.
+ */
 export default function RouteMonitor() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
