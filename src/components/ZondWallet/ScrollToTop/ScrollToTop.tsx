1c0968a5f828a51955a874e33c69cf89231caa8c
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-05 12:02:00 +0400

    feat: adds route memory to restore the last visit

--- src/components/ZondWallet/ScrollToTop/ScrollToTop.tsx
+++ src/components/ZondWallet/ScrollToTop/ScrollToTop.tsx
@@ -1,11 +1,21 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function ScrollToTop() {
+  const navigate = useNavigate();
   const { pathname } = useLocation();
 
   useEffect(() => {
+    const activePage = localStorage.getItem("ACTIVE_PAGE");
+    if (activePage) {
+      localStorage.removeItem("ACTIVE_PAGE");
+      navigate(activePage);
+    }
+  }, []);
+
+  useEffect(() => {
     window.scrollTo(0, 0);
+    localStorage.setItem("ACTIVE_PAGE", pathname);
   }, [pathname]);
 
   return null;
