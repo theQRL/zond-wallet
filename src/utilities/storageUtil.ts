6e320fbe94d2614d26094336f8f83efb8a6a31d1
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-06 20:14:30 +0400

    feat: adds transaction values to storage util

--- src/utilities/storageUtil.ts
+++ src/utilities/storageUtil.ts
@@ -4,13 +4,52 @@ const ACTIVE_PAGE_IDENTIFIER = "ACTIVE_PAGE";
 const BLOCKCHAIN_SELECTION_IDENTIFIER = "BLOCKCHAIN_SELECTION";
 const ACTIVE_ACCOUNT_IDENTIFIER = "ACTIVE_ACCOUNT";
 const ACCOUNT_LIST_IDENTIFIER = "ACCOUNT_LIST";
+const TRANSACTION_VALUES_IDENTIFIER = "TRANSACTION_VALUES";
 
 type BlockchainType = keyof typeof ZOND_PROVIDER;
+type TransactionValuesType = {
+  receiverAddress?: string;
+  amount?: number;
+  mnemonicPhrases?: string;
+};
 
 /**
  * A utility for storing and retrieving states of different components to and from the browser storage.
  */
 class StorageUtil {
+  /**
+   * A function for storing the transaction state values, so that the user need not fill in the field values if the extension is closed and opened again.
+   * Call the getTransactionValues fuction to retieve the stored value.
+   */
+  static setTransactionValues(transactionValues: TransactionValuesType) {
+    const transactionValuesWithDefaultValues = {
+      receiverAddress: transactionValues.receiverAddress ?? "",
+      amount: transactionValues.amount ?? 0,
+      mnemonicPhrases: transactionValues.mnemonicPhrases ?? "",
+    };
+    localStorage.setItem(
+      TRANSACTION_VALUES_IDENTIFIER,
+      JSON.stringify(transactionValuesWithDefaultValues),
+    );
+  }
+
+  static getTransactionValues() {
+    let transactionValues = {
+      receiverAddress: "",
+      amount: 0,
+      mnemonicPhrases: "",
+    };
+
+    const storedTransactionValues = localStorage.getItem(
+      TRANSACTION_VALUES_IDENTIFIER,
+    );
+    if (storedTransactionValues) {
+      transactionValues = JSON.parse(storedTransactionValues);
+    }
+
+    return transactionValues;
+  }
+
   /**
    * A function for storing the accounts created and imported within the zond wallet extension.
    * Call the getAccountList function to retrieve the stored value.
@@ -62,7 +101,7 @@ class StorageUtil {
   }
 
   static getBlockChain() {
-    const DEFAULT_BLOCKCHAIN = ZOND_PROVIDER.TEST_NET.id;
+    const DEFAULT_BLOCKCHAIN = ZOND_PROVIDER.MAIN_NET.id;
     return (localStorage.getItem(BLOCKCHAIN_SELECTION_IDENTIFIER) ??
       DEFAULT_BLOCKCHAIN) as BlockchainType;
   }
