a8fb2131742efda4f29a261e4f8b3e7de5285196
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-07 18:48:06 +0400

    feat: adds connection failed dialog in home

---
+++ src/components/ZondWallet/Body/Home/ConnectionFailed/ConnectionFailed.tsx
@@ -0,0 +1,15 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/UI/Alert";
+import { WifiOff } from "lucide-react";
+
+export const ConnectionFailed = () => {
+  return (
+    <Alert>
+      <WifiOff className="h-4 w-4" />
+      <AlertTitle>Connection failed</AlertTitle>
+      <AlertDescription>
+        Please ensure that you are connected to the internet, and the blockchain
+        is running.
+      </AlertDescription>
+    </Alert>
+  );
+};
--- src/components/ZondWallet/Body/Home/Home.tsx
+++ src/components/ZondWallet/Body/Home/Home.tsx
@@ -3,6 +3,7 @@ import { useStore } from "@/stores/store";
 import { Loader } from "lucide-react";
 import { observer } from "mobx-react-lite";
 import { lazy } from "react";
+import { ConnectionFailed } from "./ConnectionFailed/ConnectionFailed";
 
 const AccountCreateImport = withSuspense(
   lazy(
@@ -37,14 +38,14 @@ const Home = observer(() => {
   return (
     <>
       <BackgroundVideo />
-      <div className="relative z-10 flex w-full flex-col items-center gap-4 p-8">
+      <div className="relative z-10 flex w-full flex-col items-center gap-8 p-8">
         <img className="h-16 w-16" src="qrl-icon.png" />
         {isLoading ? (
           <Loader className="animate-spin text-foreground" size="32" />
         ) : (
           <>
             <ConnectionBadge />
-            {isConnected && <AccountCreateImport />}
+            {isConnected ? <AccountCreateImport /> : <ConnectionFailed />}
           </>
         )}
       </div>
