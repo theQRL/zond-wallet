67c3e3da7a66dcc6585ad52501851a5e62a67cd3
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-08 23:38:24 +0400

    docs: adds jsdoc for string util

--- src/utilities/stringUtil.ts
+++ src/utilities/stringUtil.ts
@@ -1,4 +1,10 @@
+/**
+ * A utility for handling string related operations
+ */
 class StringUtil {
+  /**
+   * A function for splitting the address with spaces between them, making the address more readable.
+   */
   static getSplitAddress(accountAddress: string, splitLength: number = 4) {
     const prefix = accountAddress.substring(0, 2);
     const idSplit: string[] = [];
