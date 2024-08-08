88d908c7f261a94e9662348dc760a4a0ca240fa1
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-08 19:58:08 +0400

    refactor: removes react fragment

--- src/components/ZondWallet/Body/AccountDetails/AccountDetails.tsx
+++ src/components/ZondWallet/Body/AccountDetails/AccountDetails.tsx
@@ -115,114 +115,110 @@ export const AccountDetails = observer(() => {
   return (
     <Form {...form}>
       <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
-        <>
-          <img
-            className="fixed z-0 h-96 w-96 -translate-x-8 scale-150 overflow-hidden opacity-30"
-            src="tree.svg"
-          />
-          <div className="relative z-10 p-8">
-            <Card className="w-full">
-              <CardHeader>
-                <CardTitle>Active account</CardTitle>
-              </CardHeader>
-              <CardContent className="space-y-8">
-                <div className="flex flex-col gap-2">
-                  <div>Account address</div>
-                  <div className="font-bold text-secondary">{`${prefix} ${addressSplit.join(" ")}`}</div>
-                </div>
-                <div className="flex flex-col gap-2">
-                  <div>Available amount</div>
-                  <div className="font-bold text-secondary">
-                    {accountBalance}
-                  </div>
-                </div>
-                <Separator />
-                <CardTitle>Make a transaction</CardTitle>
+        <img
+          className="fixed z-0 h-96 w-96 -translate-x-8 scale-150 overflow-hidden opacity-30"
+          src="tree.svg"
+        />
+        <div className="relative z-10 p-8">
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle>Active account</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-8">
+              <div className="flex flex-col gap-2">
+                <div>Account address</div>
+                <div className="font-bold text-secondary">{`${prefix} ${addressSplit.join(" ")}`}</div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div>Available amount</div>
+                <div className="font-bold text-secondary">{accountBalance}</div>
+              </div>
+              <Separator />
+              <CardTitle>Make a transaction</CardTitle>
+              <FormField
+                control={control}
+                name="receiverAddress"
+                render={({ field }) => (
+                  <FormItem>
+                    <Label>Send to</Label>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        autoComplete="off"
+                        disabled={isSubmitting}
+                        placeholder="Receiver address"
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Receiver&apos;s public address (0x)
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-start gap-4">
                 <FormField
                   control={control}
-                  name="receiverAddress"
+                  name="amount"
                   render={({ field }) => (
                     <FormItem>
-                      <Label>Send to</Label>
+                      <Label>Amount</Label>
                       <FormControl>
                         <Input
                           {...field}
                           autoComplete="off"
                           disabled={isSubmitting}
-                          placeholder="Receiver address"
+                          placeholder="Amount"
+                          type="number"
                         />
                       </FormControl>
-                      <FormDescription>
-                        Receiver&apos;s public address (0x)
-                      </FormDescription>
+                      <FormDescription>Amount to send</FormDescription>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
-                <div className="flex items-start gap-4">
-                  <FormField
-                    control={control}
-                    name="amount"
-                    render={({ field }) => (
-                      <FormItem>
-                        <Label>Amount</Label>
-                        <FormControl>
-                          <Input
-                            {...field}
-                            autoComplete="off"
-                            disabled={isSubmitting}
-                            placeholder="Amount"
-                            type="number"
-                          />
-                        </FormControl>
-                        <FormDescription>Amount to send</FormDescription>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <div className="w-8 pt-8 text-lg">QRL</div>
-                </div>
-                <FormField
-                  control={control}
-                  name="mnemonicPhrases"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label>Mnemonic phrases</Label>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          autoComplete="off"
-                          disabled={isSubmitting}
-                          placeholder="Mnemonic phrases"
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        Your secret mnemonic phrases
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <GasFeeNotice
-                  from={accountAddress}
-                  to={watch().receiverAddress}
-                  value={watch().amount}
-                  isSubmitting={isSubmitting}
-                />
-              </CardContent>
-              <CardFooter>
-                <Button disabled={isSubmitting || !isValid} className="w-full">
-                  {isSubmitting ? (
-                    <Loader className="mr-2 h-4 w-4 animate-spin" />
-                  ) : (
-                    <Send className="mr-2 h-4 w-4" />
-                  )}
-                  Send{isSubmitting ? "ing" : ""} quanta
-                </Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </>
+                <div className="w-8 pt-8 text-lg">QRL</div>
+              </div>
+              <FormField
+                control={control}
+                name="mnemonicPhrases"
+                render={({ field }) => (
+                  <FormItem>
+                    <Label>Mnemonic phrases</Label>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        autoComplete="off"
+                        disabled={isSubmitting}
+                        placeholder="Mnemonic phrases"
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Your secret mnemonic phrases
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <GasFeeNotice
+                from={accountAddress}
+                to={watch().receiverAddress}
+                value={watch().amount}
+                isSubmitting={isSubmitting}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button disabled={isSubmitting || !isValid} className="w-full">
+                {isSubmitting ? (
+                  <Loader className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  <Send className="mr-2 h-4 w-4" />
+                )}
+                Send{isSubmitting ? "ing" : ""} quanta
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
       </form>
     </Form>
   );
