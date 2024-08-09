76ef9ae765c2431f0852ddbf4d58e8543732c68d
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-09 11:13:07 +0400

    feat: checks network connection on restoring page

--- src/components/ZondWallet/RouteMonitor/RouteMonitor.tsx
+++ src/components/ZondWallet/RouteMonitor/RouteMonitor.tsx
@@ -1,4 +1,7 @@
+import { ROUTES } from "@/router/router";
+import { useStore } from "@/stores/store";
 import StorageUtil from "@/utilities/storageUtil";
+import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -6,16 +9,22 @@ import { useLocation, useNavigate } from "react-router-dom";
  * An empty component that stays inside the ZondWallet component to watch for changing routes.
  * This component takes care of scrolling the screen to top on route change and ensures the user is redirected to the same page on opening the extension.
  */
-export default function RouteMonitor() {
+const RouteMonitor = observer(() => {
+  const { zondStore } = useStore();
+  const { zondConnection } = zondStore;
+  const { isConnected } = zondConnection;
+
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
   useEffect(() => {
     const activePage = StorageUtil.getActivePage();
-    if (activePage) {
+    if (activePage && isConnected) {
       navigate(activePage);
+    } else {
+      navigate(ROUTES.HOME);
     }
-  }, []);
+  }, [isConnected]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -23,4 +32,6 @@ export default function RouteMonitor() {
   }, [pathname]);
 
   return null;
-}
+});
+
+export default RouteMonitor;
