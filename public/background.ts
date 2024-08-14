b05aa0f2a1559d79384f064d5b03c2f210294945
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-14 19:25:56 +0400

    refactor: updates background ts

--- public/background.ts
+++ public/background.ts
@@ -1,11 +1,1 @@
-import browser from "webextension-polyfill";
-
-console.log("Background");
-
-browser.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
-});
-
-browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-  console.log(tabs[0].url);
-});
+console.log("Background service worker script running.");
