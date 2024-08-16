d3f1325f96d39a5b86b1db7fa0300402d71ddc08
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-16 13:00:35 +0400

    test: updates jest config

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,11 @@ const config: Config = {
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^webextension-polyfill$": "<rootDir>/src/__mocks__/browser.ts",
+  },
+  setupFiles: ["./jest.setup.ts"],
 };
 
 export default config;
