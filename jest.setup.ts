3e79cb2a4da51cb05237729d31deb6f6c4a42588
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-16 16:07:51 +0400

    test: adds scrollto jest test

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,11 @@
 import { jest } from "@jest/globals";
 
 Object.defineProperty(window, "matchMedia", {
-  writable: true,
   value: jest.fn().mockImplementation((query) => ({
     matches: true,
   })),
+});
+
+Object.defineProperty(window, "scrollTo", {
+  value: jest.fn().mockImplementation((x, y) => ({})),
 });
