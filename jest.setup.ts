f84aa75e0f2a0302bde3d1f764d1195702e53a15
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-16 12:55:10 +0400

    test: adds jest setup

---
+++ jest.setup.ts
@@ -0,0 +1,9 @@
+import { jest } from "@jest/globals";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: true,
+    media: query,
+  })),
+});
