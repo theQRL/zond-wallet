959fcba49fb38fac4fdaa979aa15d25f9b0e14b0
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-17 17:03:13 +0400

    test: adds mockstore for testing

---
+++ src/__mocks__/mockedStore.ts
@@ -0,0 +1,56 @@
+import { StoreType } from "@/stores/store";
+import deepmerge from "deepmerge";
+
+type IsObject<T> = T extends object ? true : false;
+type DeepPartial<T> = {
+  [P in keyof T]?: IsObject<T[P]> extends true ? DeepPartial<T[P]> : T[P];
+};
+type OverrideStoreType = DeepPartial<StoreType>;
+
+const mockedStoreValues: StoreType = {
+  settingsStore: { isDarkMode: true, theme: "dark" },
+  zondStore: {
+    activeAccount: {
+      accountAddress: "0x2090E9F38771876FB6Fc51a6b464121d3cC093A1",
+    },
+    zondAccounts: {
+      isLoading: false,
+      accounts: [],
+    },
+    zondConnection: {
+      isConnected: true,
+      isLoading: false,
+      blockchain: "DEV",
+      zondNetworkName: "Zond Local Node",
+    },
+    zondInstance: undefined,
+    fetchAccounts: async () => {},
+    fetchZondConnection: async () => {},
+    getAccountBalance: (accountAddress: string) => {
+      accountAddress;
+      return "0 QRL";
+    },
+    initializeBlockchain: async () => {},
+    selectBlockchain: async (selectedBlockchain: string) => {
+      selectedBlockchain;
+    },
+    setActiveAccount: async () => {},
+    signAndSendTransaction: async (
+      from: string,
+      to: string,
+      value: number,
+      mnemonicPhrases: string,
+    ) => {
+      from;
+      to;
+      value;
+      mnemonicPhrases;
+      return { transactionReceipt: undefined, error: "" };
+    },
+    validateActiveAccount: async () => {},
+  },
+};
+
+export const mockedStore = (overrideStoreValues: OverrideStoreType = {}) => {
+  return deepmerge(mockedStoreValues, overrideStoreValues) as StoreType;
+};
