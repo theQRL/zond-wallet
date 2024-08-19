6149a9a03d78b20c06128aba76fa38690b8ba667
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-19 17:59:57 +0400

    test: adds test case for body component

---
+++ src/components/ZondWallet/Body/__tests__/Body.test.tsx
@@ -0,0 +1,36 @@
+import { mockedStore } from "@/__mocks__/mockedStore";
+import { StoreProvider } from "@/stores/store";
+import { describe, expect, it, jest } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Body from "../Body";
+
+jest.mock("@/router/router", () => ({
+  ROUTES: { HOME: "/" },
+}));
+jest.mock("react-router-dom", () => {
+  const originalModule =
+    jest.requireActual<typeof import("react-router-dom")>("react-router-dom");
+  return {
+    __esModule: true,
+    ...originalModule,
+    Outlet: () => <div>Mocked Outlet</div>,
+  };
+});
+
+describe("Body", () => {
+  const renderComponent = (mockedStoreValues = mockedStore()) =>
+    render(
+      <StoreProvider value={mockedStoreValues}>
+        <MemoryRouter>
+          <Body />
+        </MemoryRouter>
+      </StoreProvider>,
+    );
+
+  it("should render the router outlet in the body", () => {
+    renderComponent();
+
+    expect(screen.getByText("Mocked Outlet")).toBeInTheDocument();
+  });
+});
