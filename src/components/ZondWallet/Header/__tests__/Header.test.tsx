437b6fde7b2d66670a2a45efa3987231c6944c9c
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-19 15:45:49 +0400

    test: adds test cases for header component

---
+++ src/components/ZondWallet/Header/__tests__/Header.test.tsx
@@ -0,0 +1,54 @@
+import { mockedStore } from "@/__mocks__/mockedStore";
+import { StoreProvider } from "@/stores/store";
+import { afterEach, describe, expect, it, jest } from "@jest/globals";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Header from "../Header";
+
+jest.mock("@/router/router", () => ({
+  ROUTES: { HOME: "/" },
+}));
+jest.mock(
+  "@/components/ZondWallet/Header/ZondWalletLogo/ZondWalletLogo",
+  () => () => <div>Mocked Zond Wallet Logo</div>,
+);
+jest.mock(
+  "@/components/ZondWallet/Header/AccountBadge/AccountBadge",
+  () => () => <div>Mocked Account Badge</div>,
+);
+
+describe("Header", () => {
+  afterEach(cleanup);
+
+  const renderComponent = (mockedStoreValues = mockedStore()) =>
+    render(
+      <StoreProvider value={mockedStoreValues}>
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      </StoreProvider>,
+    );
+
+  it("should render the zond wallet logo component and the connection badge component", async () => {
+    renderComponent();
+
+    await waitFor(() => {
+      expect(screen.getByText("Mocked Zond Wallet Logo")).toBeInTheDocument();
+      expect(screen.getByText("Mocked Account Badge")).toBeInTheDocument();
+    });
+  });
+
+  it("should not render the connection badge component if the connection status is false", async () => {
+    const mockedStoreValues = mockedStore({
+      zondStore: { zondConnection: { isConnected: false } },
+    });
+    renderComponent(mockedStoreValues);
+
+    await waitFor(() => {
+      expect(screen.getByText("Mocked Zond Wallet Logo")).toBeInTheDocument();
+      expect(
+        screen.queryByText("Mocked Account Badge"),
+      ).not.toBeInTheDocument();
+    });
+  });
+});
