0cd31278ad662b5876b882e1cabc837e45255118
Shaheer Shukur <shaheershukur@gmail.com>
2024-08-19 22:21:12 +0400

    test: adds test cases for import account

---
+++ src/components/ZondWallet/Body/ImportAccount/__tests__/ImportAccount.test.tsx
@@ -0,0 +1,83 @@
+import { mockedStore } from "@/__mocks__/mockedStore";
+import { StoreProvider } from "@/stores/store";
+import { afterEach, describe, expect, it, jest } from "@jest/globals";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
+import ImportAccount from "../ImportAccount";
+
+jest.mock("@/router/router", () => ({
+  ROUTES: { HOME: "/" },
+}));
+jest.mock("@/components/ZondWallet/RouteMonitor/RouteMonitor", () => () => (
+  <div>Mocked Route Monitor</div>
+));
+
+describe("ImportAccount", () => {
+  afterEach(cleanup);
+
+  const renderComponent = (mockedStoreValues = mockedStore()) =>
+    render(
+      <StoreProvider value={mockedStoreValues}>
+        <MemoryRouter>
+          <ImportAccount />
+        </MemoryRouter>
+      </StoreProvider>,
+    );
+
+  it("should render the import account component with field and button", async () => {
+    renderComponent();
+
+    await waitFor(() => {
+      expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
+        "Import an existing account",
+      );
+      expect(
+        screen.getByRole("textbox", { name: "mnemonicPhrases" }),
+      ).toBeInTheDocument();
+      expect(screen.getByRole("paragraph")).toHaveTextContent(
+        "Paste the mnemonic phrases",
+      );
+      expect(
+        screen.getByPlaceholderText("Mnemonic Phrases"),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("button", { name: "Import account" }),
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("should render the import account button disabled initially and should be enabled when the input is entered", async () => {
+    renderComponent();
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("button", { name: "Import account" }),
+      ).toBeDisabled();
+    });
+    await userEvent.type(
+      screen.getByRole("textbox", { name: "mnemonicPhrases" }),
+      "knight paddy india glow play chew lame mature sock ill deadly olive blink marble breach hey mile mature tacit mean polo crawl khaya stud number speed viking windy jump subtle mildew sewage",
+    );
+    expect(
+      screen.getByRole("button", { name: "Import account" }),
+    ).toBeEnabled();
+  });
+
+  it("should call the submit callback on clicking the import account button", async () => {
+    renderComponent();
+    const handleOnSubmitMock = jest.fn();
+    await waitFor(async () => {
+      await userEvent.type(
+        screen.getByRole("textbox", { name: "mnemonicPhrases" }),
+        "knight paddy india glow play chew lame mature sock ill deadly olive blink marble breach hey mile mature tacit mean polo crawl khaya stud number speed viking windy jump subtle mildew sewage",
+      );
+      screen.getByRole("form", { name: "importAccount" }).onsubmit =
+        handleOnSubmitMock;
+      const button = screen.getByRole("button", { name: "Import account" });
+      await userEvent.click(button);
+    });
+
+    expect(handleOnSubmitMock).toHaveBeenCalledTimes(1);
+  });
+});
